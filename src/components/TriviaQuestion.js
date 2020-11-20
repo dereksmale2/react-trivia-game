5a894a56c3489ac02b5c8df05cd85aa6750a6d08
Derek Smale <dereksmale2@gmail.com>
2020-11-20 13:28:24 -0500

    add view all questions within a category

--- src/components/TriviaQuestion.js
+++ src/components/TriviaQuestion.js
@@ -1,12 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { isEmpty } from 'lodash'
 
-const TriviaQuestion = ({ category }) => {
+// https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+const shuffleArray = (array) => {
+  for (let origLocation = array.length - 1; origLocation > 0; origLocation--) {
+    const newLocation = Math.floor(Math.random() * origLocation)
+    const temp = array[origLocation]
+    array[origLocation] = array[newLocation]
+    array[newLocation] = temp
+  }
+}
+
+const TriviaQuestion = ({ question, handleAnswer }) => {
+  const [chosenAnswer, setChosenAnswer] = useState(null)
+  const [shuffledAnswers, setShuffledAnswers] = useState([])
+
+  useEffect(() => {
+    if (!isEmpty(question)) {
+      const answers = question.incorrect_answers.slice()
+      answers.push(question.correct_answer)
+      shuffleArray(answers)
+      setShuffledAnswers(answers)
+    }
+  }, [question])
+
   return (
-    <li key={category.question}>
+    <div>
       <div>
-        <b>Question:</b> {category.question}
+        <b>Question:</b>
+        <span dangerouslySetInnerHTML={{ __html: question.question }} />
       </div>
-    </li>
+      <div>
+        {shuffledAnswers.map((answer) => (
+          <form key={answer}>
+            <label>
+              <input
+                type='radio'
+                name={'answer-' + answer}
+                value={answer}
+                checked={chosenAnswer === answer}
+                onChange={() => {
+                  setChosenAnswer(answer)
+                  handleAnswer({ id: question.question, answer })
+                }}
+              />
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </label>
+          </form>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/TriviaQuestions.js
+++ src/components/TriviaQuestions.js
@@ -1,34 +1,41 @@
 /* globals fetch */
-// API token d692feac91a9e3011b480a18a4c7d65f09c3c30bde5aa9d3ddbae4cb7354f961
 
 import React, { useState, useEffect } from 'react'
 import TriviaQuestion from './TriviaQuestion'
 
 const TriviaQuestions = ({ category, clearSelectedCategory }) => {
   const [questions, setQuestions] = useState([])
-  const [answer, setAnswer] = useState('')
+  const [answers, setAnswers] = useState([])
 
   useEffect(() => {
-    fetch(
-      `https://opentdb.com/api.php?amount=10&category=${category.id}&token=d692feac91a9e3011b480a18a4c7d65f09c3c30bde5aa9d3ddbae4cb7354f961`
-    )
+    fetch(`https://opentdb.com/api.php?amount=10&category=${category.id}`)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         setQuestions(data.results)
       })
   }, [category])
+
+  const handleAnswer = (answer) => {
+    console.log(answer)
+  }
+
+  const SubmitAnswers = () => {}
 
   return (
     <div>
       <h2>{category.name}</h2>
       <button onClick={clearSelectedCategory}>Return to all categories</button>
-      <TriviaQuestion category={questions.length ? questions[0] : {}} />
-      <input
-        type='text'
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-      />
-      <input type='submit' value='Submit Answer' />
+      {questions.map((question) => (
+        <TriviaQuestion
+          question={question}
+          handleAnswer={handleAnswer}
+          key={question.id}
+        />
+      ))}
+      <button type='submit' onClick={SubmitAnswers}>
+        Submit Answers
+      </button>
     </div>
   )
 }
